69cbfa4677442898c7b89e6549488967010c154d
A-Slimani <abdullahslimani98@gmail.com>
2024-11-29 12:08:32 +1100

    fixes

--- ufc-dbt/models/example/fighter_stats.sql
+++ ufc-dbt/models/example/fighter_stats.sql
@@ -1,15 +1,20 @@
 SELECT 
     f2.r_fighter,
-    weight,
-    {{ generate_weight_classes('weight') }} AS weight_class,
-    height,
-    reach,
-    stance,
-    {{ generate_win_percentage('wins', 'losses', 'draws') }} AS win_percentage,
-    {{ generate_total_fights('wins', 'losses', 'draws') }} AS total_fights,
-    wins,
-    losses,
-    draws
+    f1.weight,
+    f1.{{ generate_weight_classes('weight') }} AS weight_class,
+    f1.height,
+    f1.reach,
+    f1.stance,
+    f1.{{ generate_win_percentage('wins', 'losses', 'draws') }} AS win_percentage,
+    f1.{{ generate_total_fights('wins', 'losses', 'draws') }} AS total_fights,
+    f1.wins,
+    f1.losses,
+    f1.draws,
+    f2.total_wins_by_dec,
+    f2.total_wins_by_unanimous_dec,
+    f2.total_wins_by_split_dec,
+    f2.total_wins_by_ko_tko,
+    f2.total_wins_by_sub
 FROM 
     {{ source('ufcdb', 'fighters') }} f1
 LEFT JOIN 
