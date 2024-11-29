3a485df7f31be6282a3a3061ba385970931a1cfc
A-Slimani <abdullahslimani98@gmail.com>
2024-11-29 15:24:55 +1100

    fixes

--- ufc-dbt/models/example/fighter_stats.sql
+++ ufc-dbt/models/example/fighter_stats.sql
@@ -10,11 +10,11 @@ SELECT
     f1.wins,
     f1.losses,
     f1.draws,
-    f2.total_wins_by_dec,
-    f2.total_wins_by_unanimous_dec,
-    f2.total_wins_by_split_dec,
-    f2.total_wins_by_ko_tko,
-    f2.total_wins_by_sub
+    CASE WHEN f2.total_wins_in_ufc_by_dec NOT NULL THEN f2.total_wins_in_ufc_by_unanimous_dec ELSE 0 END , 
+    CASE WHEN f2.total_wins_in_ufc_by_unanimous_dec NOT NULL THEN f2.total_wins_in_ufc_by_unanimous_dec ELSE 0 END , 
+    CASE WHEN f2.total_wins_in_ufc_by_split_dec NOT NULL THEN f2.total_wins_in_ufc_by_split_dec ELSE 0 END , 
+    CASE WHEN f2.total_wins_in_ufc_by_ko_tko NOT NULL THEN f2.total_wins_in_ufc_by_ko_tko ELSE 0 END , 
+    CASE WHEN f2.total_wins_in_ufc_by_sub NOT NULL THEN f2.total_wins_in_ufc_by_sub ELSE 0 END , 
 FROM 
     {{ source('ufcdb', 'fighters') }} f1
 LEFT JOIN 
