6c863dac033377b232eb12fe24ff9430d5e8edae
A-Slimani <abdullahslimani98@gmail.com>
2025-09-23 09:27:07 +1000

    fixes to dbt

--- ufcdbt/models/intermediate/int_fighter_is_active.sql
+++ ufcdbt/models/intermediate/int_fighter_is_active.sql
@@ -1,19 +1,16 @@
 WITH combined_tables AS (
   SELECT
     r_fighter_id AS "id",
-    r_fighter_name AS "full_name",
     fight_date
   FROM {{ ref('stg_fights') }} f
   UNION
   SELECT
     b_fighter_id AS "id",
-    b_fighter_name AS "full_name",
     fight_date
   FROM {{ ref('stg_fights') }} 
 )
 SELECT
   id,
-  full_name,
   MAX(fight_date) AS "last_active" 
 FROM {{ ref('stg_fights') }}
-GROUP BY id, full_name
+GROUP BY id 
