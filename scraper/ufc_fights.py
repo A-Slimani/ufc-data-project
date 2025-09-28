99bef9f248846daaa0fd9a870e770d15d82ba33b
A-Slimani <abdullahslimani98@gmail.com>
2025-09-29 09:13:24 +1000

    fixes

--- scraper/ufc_fights.py
+++ scraper/ufc_fights.py
@@ -123,7 +123,7 @@ def create_fight_table():
     b_fighter_id INTEGER REFERENCES raw_fighters(id) ON DELETE CASCADE,
     b_fighter_status TEXT,
     fight_order INTEGER,
-    round_ending INTEGER,
+    ending_rounds INTEGER,
     time TEXT,
     method TEXT,
     bout_weight TEXT,
