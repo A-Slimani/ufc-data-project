1795ed505a9955c9badd65d962eed16217ca8e0b
Abdullah Slimani <abdullahslimani98@gmail.com>
2025-10-09 15:53:21 +1100

    updates

--- sql_scripts/check_time_limit.sql
+++ sql_scripts/check_time_limit.sql
@@ -1,19 +1,32 @@
-
 WITH seconds_table AS (
   SELECT
-    id,
-    r_fighter_name,
-    b_fighter_name,
-    event_id,
     ending_round,
-    EXTRACT(EPOCH FROM ('00: ' || time)::INTERVAL)::INT,
+    EXTRACT(MINUTE FROM ('00:' || time)::INTERVAL) AS "fight_minutes",
+    EXTRACT(SECOND FROM ('00:' || time)::INTERVAL) AS "fight_seconds",
+    time,
     method,
     ground_time_seconds AS "g",
     standing_time_seconds AS "s",
     clinch_time_seconds AS "c"
   FROM 
     dbt_schema.fact_fights
+), calculated_seconds AS (
+  SELECT
+    ((ending_round - 1) * 5 * 60) + (fight_minutes * 60) + (fight_seconds) AS "total_seconds",
+    g + s AS "total_seconds_by_combined"
+  FROM seconds_table 
+), diff_table AS (
+  SELECT
+    total_seconds,
+    total_seconds_by_combined,
+    ABS(total_seconds - total_seconds_by_combined) AS "difference"
+  FROM calculated_seconds
 )
+SELECT
+  AVG(difference) AS "mean",
+  STDDEV(difference) AS "standard_deviation",
+  MAX(difference) AS "max_difference"
+FROM diff_table
 
 -- clinch included in standing
 SELECT 
@@ -32,4 +45,6 @@ SELECT g + s + c AS "total" FROM seconds_table
 WHERE g + s + c > 900
 
 -- seconds difference
-
+SELECT
+  
+FROM seconds_table
---
+++ sql_scripts/reach_standup_ratio.sql
@@ -0,0 +1,35 @@
+WITH t_values AS (
+  SELECT
+    r_fighter_id,
+    b_fighter_id,
+    standing_time_seconds AS "standing",
+    ground_time_seconds AS "ground",
+    clinch_time_seconds AS "clinch"
+  FROM dbt_schema.fact_fights f
+  LEFT JOIN dbt_schema.dim_fighters d
+  ON 
+    f.r_fighter_id = d.fighter_id 
+    AND
+    f.b_fighter_id = d.fighter_id
+), ratio_values AS (
+  SELECT 
+    r_fighter_name,
+    b_fighter_name,
+    standing - clinch AS "striking",
+    ground + clinch AS "grappling"
+  FROM t_values
+)
+-- SELECT 
+--   r_fighter_name,
+--   b_fighter_name,
+--   striking,
+--   grappling,
+--   ROUND(striking::NUMERIC / (striking + grappling), 2) AS "ratio"
+-- FROM ratio_values
+-- ORDER BY ratio
+SELECT
+  ROUND(striking::NUMERIC / (striking + grappling), 2) * 100 AS "ratio",
+  COUNT(ROUND(striking::NUMERIC / (striking + grappling), 2)) 
+FROM ratio_values
+GROUP BY ratio
+ORDER BY ratio
