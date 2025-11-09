2fd0e5a4261da22ad844ded10475eb41030c4527
Abdullah Slimani <abdullahslimani98@gmail.com>
2025-11-10 08:20:15 +1100

    updates

--- ufcdbt/models/intermediate/fighter/int_fighter_ufc_losses_by.sql
+++ ufcdbt/models/intermediate/fighter/int_fighter_ufc_losses_by.sql
@@ -7,15 +7,26 @@ WITH winning_fighter AS (
       ElSE NULL 
     END AS "fighter_id"
   FROM {{ ref('stg_fights')}}
+), counts AS (
+  SELECT 
+    a.fighter_id,
+    COUNT(method_type) AS "ufc_losses",
+    COUNT(CASE WHEN a.method_type=1 THEN 1 END) AS "ufc_losses_by_ko_tko", 
+    COUNT(CASE WHEN a.method_type=2 THEN 1 END) AS "ufc_losses_by_sub", 
+    COUNT(CASE WHEN a.method_type=3 THEN 1 END) AS "ufc_losses_by_dec" 
+  FROM winning_fighter a
+  JOIN {{ ref('stg_fighters') }} b
+  ON a.fighter_id = b.id
+  WHERE a.fighter_id IS NOT NULL
+  GROUP BY a.fighter_id
 )
-SELECT 
-  a.fighter_id,
-  COUNT(method_type) AS "ufc_losses",
-  COUNT(CASE WHEN a.method_type=1 THEN 1 END) AS "ufc_losses_by_ko_tko", 
-  COUNT(CASE WHEN a.method_type=2 THEN 1 END) AS "ufc_losses_by_sub", 
-  COUNT(CASE WHEN a.method_type=3 THEN 1 END) AS "ufc_losses_by_dec" 
-FROM winning_fighter a
-JOIN {{ ref('stg_fighters') }} b
-ON a.fighter_id = b.id
-WHERE a.fighter_id IS NOT NULL
-GROUP BY a.fighter_id
+SELECT
+  fighter_id,
+  ufc_losses,
+  ufc_losses_by_ko_tko,
+  {{ get_percentage(['ufc_losses_by_ko_tko', 'ufc_losses']) }} AS "ufc_loss_percentage_by_ko_tko", 
+  ufc_losses_by_sub,
+  {{ get_percentage(['ufc_losses_by_sub', 'ufc_losses']) }} AS "ufc_loss_percentage_by_sub", 
+  ufc_losses_by_dec,
+  {{ get_percentage(['ufc_losses_by_dec', 'ufc_losses']) }} AS "ufc_loss_percentage_by_dec" 
+FROM counts
--- ufcdbt/models/intermediate/fighter/int_fighter_ufc_wins_by.sql
+++ ufcdbt/models/intermediate/fighter/int_fighter_ufc_wins_by.sql
@@ -7,15 +7,26 @@ WITH winning_fighter AS (
       ElSE NULL 
     END AS "fighter_id"
   FROM {{ ref('stg_fights')}}
+), counts AS (
+  SELECT 
+    a.fighter_id,
+    COUNT(method_type) AS "ufc_wins",
+    COUNT(CASE WHEN a.method_type=1 THEN 1 END) AS "ufc_wins_by_ko_tko", 
+    COUNT(CASE WHEN a.method_type=2 THEN 1 END) AS "ufc_wins_by_sub", 
+    COUNT(CASE WHEN a.method_type=3 THEN 1 END) AS "ufc_wins_by_dec" 
+  FROM winning_fighter a
+  JOIN {{ ref('stg_fighters') }} b
+  ON a.fighter_id = b.id
+  WHERE a.fighter_id IS NOT NULL
+  GROUP BY a.fighter_id
 )
-SELECT 
-  a.fighter_id,
-  COUNT(method_type) AS "ufc_wins",
-  COUNT(CASE WHEN a.method_type=1 THEN 1 END) AS "ufc_wins_by_ko_tko", 
-  COUNT(CASE WHEN a.method_type=2 THEN 1 END) AS "ufc_wins_by_sub", 
-  COUNT(CASE WHEN a.method_type=3 THEN 1 END) AS "ufc_wins_by_dec" 
-FROM winning_fighter a
-JOIN {{ ref('stg_fighters') }} b
-ON a.fighter_id = b.id
-WHERE a.fighter_id IS NOT NULL
-GROUP BY a.fighter_id
+SELECT
+  fighter_id,
+  ufc_wins,
+  ufc_wins_by_ko_tko,
+  {{ get_percentage(['ufc_wins_by_ko_tko', 'ufc_wins']) }} AS "ufc_win_percentage_by_ko_tko", 
+  ufc_wins_by_sub,
+  {{ get_percentage(['ufc_wins_by_sub', 'ufc_wins']) }} AS "ufc_win_percentage_by_sub", 
+  ufc_wins_by_dec,
+  {{ get_percentage(['ufc_wins_by_dec', 'ufc_wins']) }} AS "ufc_win_percentage_by_dec" 
+FROM counts
